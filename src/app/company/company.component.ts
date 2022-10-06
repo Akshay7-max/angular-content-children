8d0eb5d6311654fa3ba1cde2f2e3138e04dc35bd
Akshay Kumbhare <52664007+Akshay7-max@users.noreply.github.com>
2022-10-06 15:25:44 +0530

    Add files via upload

---
+++ src/app/company/company.component.ts
@@ -0,0 +1,28 @@
+import { Component, ContentChildren, QueryList } from '@angular/core';
+import { EmployeeComponent } from "../employee/employee.component";
+
+@Component({
+  selector: 'app-company',
+  templateUrl: './company.component.html',
+  styleUrls: ['./company.component.css']
+})
+export class CompanyComponent {
+
+  companyname: string = "ABC Company";
+
+  @ContentChildren(EmployeeComponent) emp!: QueryList<EmployeeComponent>;
+
+  onClickMeClicked() {
+    console.log(this.emp);
+    var a = this.emp.toArray();
+    for (var i = 0; i < a.length; i++) {
+      a[i].empname = "Ram";
+    }
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
